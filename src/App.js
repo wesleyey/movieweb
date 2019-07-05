cf3619dc55060f91ed0227f7246f6caba9e5dbbf
wesley <truthotwo@gmail.com>
2019-07-05 13:36:40 +0900

    use API
    Fetch(), Json(), setState()

--- src/App.js
+++ src/App.js
@@ -6,42 +6,34 @@ class App extends Component {
   state = {};
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        movies: [
-          {
-            title: "John wick",
-            poster:
-              "https://upload.wikimedia.org/wikipedia/en/thumb/9/98/John_Wick_TeaserPoster.jpg/220px-John_Wick_TeaserPoster.jpg"
-          },
-          {
-            title: "Spiderman",
-            poster:
-              "https://www.sideshow.com/storage/product-images/904867/spider-man-upgraded-suit_marvel_gallery_5d13ad633bb71_th.jpg"
-          },
-          {
-            title: "Saw",
-            poster:
-              "https://upload.wikimedia.org/wikipedia/en/thumb/3/35/Saw4final.jpg/220px-Saw4final.jpg"
-          },
-          {
-            title: "Usual Suspect",
-            poster:
-              "https://upload.wikimedia.org/wikipedia/en/9/9c/Usual_suspects_ver1.jpg"
-          },
-          {
-            title: "shazam",
-            poster:
-              "https://upload.wikimedia.org/wikipedia/en/thumb/6/60/Shazam%21_theatrical_poster.jpg/220px-Shazam%21_theatrical_poster.jpg"
-          }
-        ]
-      });
-    }, 3000);
+    this._getMovies();
   }
 
+  _getMovies = async () => {
+    const movies = await this._callApi();
+    this.setState({
+      movies
+    });
+  };
+
+  _callApi = () => {
+    return fetch(
+      "https://yts.lt/api/v2/list_movies.json?sort_by=download_count"
+    )
+      .then(response => response.json())
+      .then(json => json.data.movies)
+      .catch(error => console.log(error));
+  };
   _renderMovie = () => {
     const movies = this.state.movies.map((movie, index) => {
-      return <Movie title={movie.title} poster={movie.poster} key={index} />;
+      //console.log(movie);
+      return (
+        <Movie
+          title={movie.title}
+          poster={movie.medium_cover_image}
+          key={index}
+        />
+      );
     });
     return movies;
   };
